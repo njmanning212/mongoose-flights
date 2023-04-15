985188c47bf47ac06b3a928ce346cd2a87107cde
Nicholas Manning <njmanning212@gmail.com>
2023-04-15 14:51:18 -0500

    ad flight to database with POST /flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -4,9 +4,23 @@ function newflight (req, res) {
   res.render('flights/new', {
     title: 'Add Flight'
   })
+}
 
+function create (req, res) {
+  if (req.body.flightN0) {
+    req.body.flightN0 = parseInt(req.body.flightN0)
+  }
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect('/flights/new')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/flights/new')
+  })
 }
 
 export {
   newflight as new,
+  create,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -5,5 +5,6 @@ const router = Router()
 
 // GET localhost:3000/users
 router.get('/new', flightCtrl.new)
+router.post('/', flightCtrl.create)
 
 export { router }
